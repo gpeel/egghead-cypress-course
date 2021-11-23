52b0ce5380a0aaefe762b56ce164a73e2fbf4026
Gauthier PEEL <gauthier.peel@gmail.com>
2021-11-23 17:56:13 +0100

    git

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -1,12 +1,13 @@
 describe('App initialization', () => {
-  it('Displays todos from API on load', () => {
-    cy.server()
-    cy.route('GET', '/api/todos', 'fixture:todos').as('load')
+    it('Displays todos from API on load', () => {
+        cy.server();
+        cy.route('GET', '/api/todos', 'fixture:todos').as('load');
+        // as is an alias for the route
 
-    cy.visit('/')
+        cy.visit('/');
 
-    cy.wait('@load')
+        cy.wait('@load'); // waiting for the alias route @ + load
 
-    cy.get('.todo-list li').should('have.length', 4)
-  })
-})
+        cy.get('.todo-list li').should('have.length', 4);
+    });
+});
