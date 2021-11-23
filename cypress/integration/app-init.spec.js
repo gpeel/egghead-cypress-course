749c9b8266924e74af0fafdedfbdaa9d7820e391
Gauthier PEEL <gauthier.peel@gmail.com>
2021-11-23 17:41:00 +0100

    git

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -1,5 +1,7 @@
 describe('App initialization', () => {
   it('Displays todos from API on load', () => {
+
+    // intercepting HTPP
     cy.server()
     cy.route('GET', '/api/todos', [
       { id: 1, name: 'One', isComplete: false },
@@ -7,6 +9,8 @@ describe('App initialization', () => {
       { id: 3, name: 'Three', isComplete: false },
       { id: 4, name: 'Four', isComplete: false }
     ])
+
+
     cy.visit('/')
     cy.get('.todo-list li').should('have.length', 4)
   })
